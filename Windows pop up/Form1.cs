5ab5e8fddc05e1bd2823d43c167c73a993369204
qais <qais_1990@live.co.uk>
2023-03-20 12:50:05 +0000

    Add project files.

---
+++ Windows pop up/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows_pop_up
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("file:///C:/Users/qais_/source/repos/ConsoleApp1/Windows%20pop%20up/Windows%20pop%20up/Resources/Contact%20Page.html");
+        }
+    }
+}
